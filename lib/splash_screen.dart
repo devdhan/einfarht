956711330dcbbcd274e5497f69eebc707c071023
Daniel <danielbolaji99@gmail.com>
2024-12-23 10:33:20 +0100

    splash screen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -5,6 +5,26 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/maskgroup.png'),
+            const Text(
+              'EINFARHT',
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                color: Color(0x9C4865EF),
+                fontSize: 32,
+                letterSpacing: 4,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
